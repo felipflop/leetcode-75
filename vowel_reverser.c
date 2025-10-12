006c55d4af28982fae9b9844a1db6947f75ed6db
felipflop <felipe.canazil@gmail.com>
2025-10-11 20:21:58 -0700

    fix: renaming + reformatting

--- vowel_reverser.c
+++ vowel_reverser.c
@@ -2,26 +2,22 @@
 #include <string.h>
 #include <stdbool.h>
 
-#define VOWELS 10
-
 char* reverse_vowels(char *);
-bool is_vowel(char);
+
+static size_t count_vowels(char *);
+static bool   is_vowel(unsigned char);
+static void   collect_vowels(char *, char *, size_t *);
 
 char* reverse_vowels(char* s) {
     if (!s) return NULL;
-    size_t len = strlen(s);
 
     /* Count number of vowels */
-    size_t vowel_count = 0;
-    for (size_t i = 0; i < len; ++i) {
-        if (is_vowel(s[i]))
-            ++vowel_count; 
-    }
+    size_t n_vowels = count_vowels(s);
 
-    if (vowel_count == 0) return s;
+    if (n_vowels == 0) return s;
 
-    char *vals = malloc(vowel_count * sizeof(char));        
-    size_t *idxs = malloc(vowel_count * sizeof(size_t));    
+    char* vals = malloc(n_vowels * sizeof(char));        
+    size_t* idxs = malloc(n_vowels * sizeof(size_t));    
 
     if (!vals || !idxs) {                                  
         free(vals);
@@ -29,18 +25,11 @@ char* reverse_vowels(char* s) {
         return s; // return original string to avoid crash 
     }
 
-    size_t j = 0;
-    for (size_t i = 0; i < len; ++i) {
-        if (is_vowel(s[i])) {
-            vals[j] = s[i];   
-            idxs[j] = i;      
-            ++j;
-        }
-    }
+    collect_vowels(s, vals, idxs);
 
     /* Reverse vowels */
-    size_t k = vowel_count - 1;
-    for (size_t i = 0; i < vowel_count; ++i) {
+    size_t k = n_vowels - 1;
+    for (size_t i = 0; i < n_vowels; ++i) {
         s[idxs[i]] = vals[k--];
     }
 
@@ -50,16 +39,42 @@ char* reverse_vowels(char* s) {
     return s;
 }
 
-bool is_vowel(char s) {
+static size_t count_vowels(char* str) {
+    size_t vowel_count = 0;
+    size_t len = strlen(str);
+
+    for (size_t i = 0; i < len; ++i) {
+        if (is_vowel(str[i]))
+            ++vowel_count; 
+    }
+
+    return vowel_count;
+}
+
+static bool is_vowel(unsigned char c) {
     const char vowel_lut[] = {
         'a', 'e', 'i', 'o', 'u', 
         'A', 'E', 'I', 'O', 'U', 
     };
+    size_t lut_len = sizeof(vowel_lut)/sizeof(vowel_lut[0]);
 
-    for (int i = 0; i < VOWELS; ++i) {
-        if (s == vowel_lut[i])
+    for (size_t i = 0; i < lut_len; ++i) {
+        if (c == vowel_lut[i])
             return true;
     }
 
     return false;
+}
+
+static void collect_vowels(char* str, char* vals, size_t* idxs) {
+    size_t j = 0;
+    size_t len = strlen(str);
+
+    for (size_t i = 0; i < len; ++i) {
+        if (is_vowel(str[i])) {
+            vals[j] = str[i];   
+            idxs[j] = i;      
+            ++j;
+        }
+    }
 }
